8334555e70858e24d1a7d63edd4b547ed1744f02
filip culig <filip.culig@gmail.com>
2021-07-11 19:09:30 +0200

    Initializing project

---
+++ src/test/java/com/fifi/technologies/tourist/TouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifi.technologies.tourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
